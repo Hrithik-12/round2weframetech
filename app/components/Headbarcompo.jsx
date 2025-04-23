ba1f9e8ced547653a922aba4a7e5122e86f9d57d
Hrithik-12 <hrithikiit12@gmail.com>
2025-04-23 21:13:44 +0530

    accessiblity

--- app/components/Headbarcompo.jsx
+++ app/components/Headbarcompo.jsx
@@ -20,10 +20,10 @@ export default function Headbarcompo() {
 
       <div className="flex flex-wrap items-center justify-between md:justify-end gap-4 w-full md:w-auto">
         <div className="flex items-center gap-3">
-          <button className="p-2.5 bg-gray-100 rounded-lg hover:bg-gray-200 transition-colors">
+          <button aria-label="calender" className="p-2.5 bg-gray-100 rounded-lg hover:bg-gray-200 transition-colors">
             <CalendarDays className="text-indigo-950" size={20} />
           </button>
-          <button className="p-2.5 bg-gray-100 rounded-lg hover:bg-gray-200 transition-colors">
+          <button aria-label="filter" className="p-2.5 bg-gray-100 rounded-lg hover:bg-gray-200 transition-colors">
             <Filter className="text-indigo-950" size={20} />
           </button>
         </div>
